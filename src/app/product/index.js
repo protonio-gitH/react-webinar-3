0b65c023a152bf68bd0ed62c895ddd23c99228dc
protonio git-H <profiloh@mail.ru>
2024-09-26 23:47:51 +0300

    some fixes

--- src/app/product/index.js
+++ src/app/product/index.js
@@ -1,56 +1,47 @@
-import React, { useCallback, useEffect } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { cn as bem } from '@bem-react/classname';
-import Head from '../../components/head';
-import PageLayout from '../../components/page-layout';
-import useStore from '../../store/use-store';
-import useSelector from '../../store/use-selector';
-import BasketTool from '../../components/basket-tool';
-import { Link, useParams } from 'react-router-dom';
 import './style.css';
-import ProductBody from '../../components/product-body';
-import { numberFormat } from '../../utils';
 
-function Product() {
-  const store = useStore();
-  const params = useParams();
-  const cn = bem('Product');
+const ProductBody = ({ description, country, category, year, price, onAdd, id }) => {
+  const cn = bem('ProductBody');
 
-  const select = useSelector(state => ({
-    amount: state.basket.amount,
-    sum: state.basket.sum,
-    result: state.product.result,
-  }));
-
-  useEffect(() => {
-    store.actions.product.getProduct(params.id);
-  }, [params.id]);
-
-  const callbacks = {
-    // Добавление в корзину
-    addToBasket: useCallback(_id => store.actions.basket.addToBasket(_id), [store]),
-    // Открытие модалки корзины
-    openModalBasket: useCallback(() => store.actions.modals.open('basket'), [store]),
-  };
   return (
-    <PageLayout>
-      <Head title={select.result.title} />
-      <div className={cn('header')}>
-        <Link to="/" className="main-button">
-          Главная
-        </Link>
-        <BasketTool onOpen={callbacks.openModalBasket} amount={select.amount} sum={select.sum} />
-      </div>
-      <ProductBody
-        description={select.result.description}
-        country={select.result?.madeIn?.title}
-        category={select.result?.category?.title}
-        year={new Date(select.result.dateCreate).getFullYear()}
-        price={numberFormat(select.result.price)}
-        onAdd={callbacks.addToBasket}
-        id={select.result._id}
-      />
-    </PageLayout>
+    <div className={cn()}>
+      <p className={cn('about')}>{description}</p>
+      <p className={cn('country')}>
+        Страна производитель: <b>{country}</b>
+      </p>
+      <p className={cn('category')}>
+        Категория: <b>{category}</b>
+      </p>
+      <p className={cn('year')}>
+        Год выпуска: <b>{year}</b>
+      </p>
+      <p className={cn('year')}>
+        <b>Цена: {price} ₽</b>
+      </p>
+      <button onClick={() => onAdd(id)}>Добавить</button>
+    </div>
   );
-}
+};
 
-export default React.memo(Product);
+ProductBody.propTypes = {
+  description: PropTypes.string,
+  country: PropTypes.string,
+  category: PropTypes.string,
+  year: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  price: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  onAdd: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired,
+};
+
+ProductBody.defaultProps = {
+  description: 'Описание отсутствует',
+  country: 'Неизвестно',
+  category: 'Без категории',
+  year: 'Неизвестно',
+  price: '0',
+};
+
+export default React.memo(ProductBody);
